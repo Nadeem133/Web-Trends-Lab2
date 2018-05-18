4d3c7fe0cc1dc97015c81e7296e40f507c057b01
Nadeem133 <32464612+Nadeem133@users.noreply.github.com>
2018-05-18 14:05:24 -0400

    Lab2 - Part2 files

--- lab2.js
+++ lab2.js
@@ -1,16 +1,16 @@
 var days;
 (function (days) {
-    days[days["Sunday"] = 1] = "Sunday";
-    days[days["Monday"] = 2] = "Monday";
-    days[days["Tuesday"] = 3] = "Tuesday";
-    days[days["Wednesday"] = 4] = "Wednesday";
-    days[days["Thursaday"] = 5] = "Thursaday";
-    days[days["Friday"] = 6] = "Friday";
-    days[days["Saturday"] = 7] = "Saturday";
+    days[days["Sunday"] = 0] = "Sunday";
+    days[days["Monday"] = 1] = "Monday";
+    days[days["Tuesday"] = 2] = "Tuesday";
+    days[days["Wednesday"] = 3] = "Wednesday";
+    days[days["Thursday"] = 4] = "Thursday";
+    days[days["Friday"] = 5] = "Friday";
+    days[days["Saturday"] = 6] = "Saturday";
 })(days || (days = {}));
 var months;
 (function (months) {
-    months[months["Januray"] = 0] = "Januray";
+    months[months["January"] = 0] = "January";
     months[months["February"] = 1] = "February";
     months[months["March"] = 2] = "March";
     months[months["April"] = 3] = "April";
@@ -23,10 +23,38 @@ var months;
     months[months["November"] = 10] = "November";
     months[months["December"] = 11] = "December";
 })(months || (months = {}));
-console.log(days[0]);
 //html elements
+//Part 1
 var pTodayDate = document.getElementById("p--today-date");
+var buttonBirthday = document.getElementById("button--birthday");
+var pBirthdayMessage = document.getElementById("p--birthday-message");
+var inputDatePicker = document.getElementById("input--date-picker");
 //today as a date
 var today = new Date();
 console.log(today.getMonth());
 pTodayDate.innerHTML = "Today is " + days[today.getDay()] + " " + months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+//Part2
+buttonBirthday.onclick = function () {
+    //get birthdate from input tag
+    var userBDay = inputDatePicker.value;
+    var bDateObject = stringToDate(userBDay);
+    pBirthdayMessage.innerHTML = makeDateString(bDateObject);
+};
+function stringToDate(str_date) {
+    var arr = str_date.split('-');
+    return new Date(arr[0], arr[1] - 1, arr[2]);
+}
+function makeDateString(inputDate) {
+    //if today is user's brithday
+    if ((inputDate.getDate() === today.getDate())
+        &&
+            (inputDate.getMonth() === today.getMonth())) {
+        return "Happy birthday!";
+    }
+    //today is not their birthday
+    var thisYearBirthday = new Date();
+    thisYearBirthday.setDate(inputDate.getDate());
+    thisYearBirthday.setFullYear(today.getFullYear());
+    thisYearBirthday.setMonth(inputDate.getMonth());
+    return "Your birthday is " + days[thisYearBirthday.getDay()] + " " + months[thisYearBirthday.getMonth()] + " " + thisYearBirthday.getDate() + ", " + thisYearBirthday.getFullYear();
+}
--- lab2.ts
+++ lab2.ts
@@ -24,10 +24,13 @@ enum months{
 
 }
 
-console.log(days[0];
-
     //html elements
+    //Part 1
     let pTodayDate = document.getElementById("p--today-date");
+    let buttonBirthday = document.getElementById("button--birthday");
+    let pBirthdayMessage = document.getElementById("p--birthday-message");
+    let inputDatePicker = document.getElementById("input--date-picker");
+
 
     //today as a date
     let today : Date = new Date();
@@ -37,6 +40,37 @@ console.log(days[0];
     pTodayDate.innerHTML = `Today is ${days[today.getDay()]} ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
 
     
+    //Part2
+    buttonBirthday.onclick = function(){
+        //get birthdate from input tag
+        let userBDay : string = inputDatePicker.value;
+        let bDateObject : Date = stringToDate(userBDay);
+        pBirthdayMessage.innerHTML=makeDateString(bDateObject);
+        
+    }
+    function stringToDate(str_date:string){
+        let arr=str_date.split('-')
+        return new Date(arr[0],arr[1]-1,arr[2])
+    }
+    function makeDateString( inputDate : Date) : string{
 
+        //if today is user's brithday
+    if((inputDate.getDate() === today.getDate()) 
+    && 
+    (inputDate.getMonth() === today.getMonth())){
+        return `Happy birthday!`;
+    }
+    
+
+    //today is not their birthday
+    let thisYearBirthday = new Date();
+    thisYearBirthday.setDate( inputDate.getDate() );
+    thisYearBirthday.setFullYear(today.getFullYear() );
+    thisYearBirthday.setMonth( inputDate.getMonth() );
+
+    return `Your birthday is ${days[thisYearBirthday.getDay()] } ${months[thisYearBirthday.getMonth()]} ${thisYearBirthday.getDate() }, ${thisYearBirthday.getFullYear()}`
+
+    }
+    
 
 
